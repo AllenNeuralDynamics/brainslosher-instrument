b3e3db8e638981af2880fafdac1562ff0f19b312
Micah Woodard <micah.woodard@alleninstitute.org>
2026-02-23 16:04:16 -0800

    pulls latest release files

--- src/brainslosher-instrument/main.py
+++ src/brainslosher-instrument/main.py
@@ -5,10 +5,12 @@ from pathlib import Path
 import urllib.request
 import zipfile
 import json
+import logging
 
 UI_REPO = "AllenNeuralDynamics/brainslosher-web-ui"
 FILE_NAME = "ui.zip"
 UI_DIR = Path("ui")
+VERSION_FILE = Path("ui/.version")
 
 def get_latest_release_asset_url():
     url = f"https://api.github.com/repos/{UI_REPO}/releases/latest"
@@ -18,29 +20,39 @@ def get_latest_release_asset_url():
     
     for asset in release["assets"]:
         if asset["name"] == FILE_NAME:
-            return asset["browser_download_url"]
+            return asset["browser_download_url"], release["tag_name"]
 
 def fetch_ui():
-    if UI_DIR.exists():
-        print("UI already present, skipping download")
+    latest_url, latest_tag = get_latest_release_asset_url()
+    
+    if VERSION_FILE.exists() and VERSION_FILE.read_text() == latest_tag:
+        print(f"UI is up to date ({latest_tag})")
         return
     
-    print("Downloading latest UI release...")
-    url = get_latest_release_asset_url()
-    urllib.request.urlretrieve(url, FILE_NAME)
+    print(f"Downloading UI {latest_tag}...")
+    urllib.request.urlretrieve(latest_url, FILE_NAME)
     
     with zipfile.ZipFile(FILE_NAME) as z:
         z.extractall(UI_DIR)
     
+    VERSION_FILE.write_text(latest_tag)
     Path(FILE_NAME).unlink()
     print("UI ready")
+    
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--instrument-config", required=True)
     parser.add_argument("--ui-config", required=True)
     parser.add_argument("--log-level", default="INFO", choices=["INFO", "DEBUG"])
+    
     args = parser.parse_args()
+    logger = logging.getLogger()
+
+    # Override console log level if specified.
+    for handler in logger.handlers:
+        if handler.get_name() == 'console':
+            handler.setLevel(args.log_level)
 
     fetch_ui()
 
